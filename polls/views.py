baea02ec58eb27990272b4a183caef20f41bc1e1
akrzy <akrzy@users.noreply.github.com>
2024-01-21 10:28:57 +0000

    Changed index function

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
@@ -16,4 +19,3 @@ def result(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-    
